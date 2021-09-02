da5d239bf7b5b9b09e5a6873807ad434373b40ab
Nooshin <nooshinas1988@gmail.com>
2021-09-02 03:40:19 +0200

    build(db): connect to database

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,25 @@ import express from "express";
 import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
 import { GraphQLSchema } from "graphql";
+import { createConnection } from "typeorm";
+import dotenv from "dotenv";
 
 const main = async () => {
+  dotenv.config();
+
+  await createConnection({
+    type: "mysql",
+    name: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 3306 || process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    logging: true,
+    synchronize: false,
+    entities: [],
+  });
+
   const app = express();
   app.use(cors());
   app.use(express.json());
