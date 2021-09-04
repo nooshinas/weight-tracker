9493c99be5b296f4ff23f6e40647dbd919d88f54
Nooshin <nooshinas1988@gmail.com>
2021-09-04 11:50:22 +0200

    feat(frontend): add loading component

---
+++ client/src/components/loading/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading-overlay">
+      <div className="loading" />
+    </div>
+  );
+};
+
+export default Loading;
